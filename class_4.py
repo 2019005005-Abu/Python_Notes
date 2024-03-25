95863a3bbf4a67e3cd6bdd19252348e9bee31b60
Rifat <rifatyou2686@gmail.com>
2024-03-26 00:21:41 +0600

    Membership operator and round added

--- class_4.py
+++ class_4.py
@@ -1,2 +1,16 @@
 # r W >
-class_4('I am  python Developer')
+class_4 = print
+class_4("I am  python Developer")
+
+##membership operator
+
+a = [100, 200]
+class_4(100 in a)
+cou = "Bangladeshi"
+class_4("a" in cou)
+class_4("n" not in cou)
+a1 = 6.77475675675675854
+a_1 = round(a1)
+class_4(a_1)
+class_4("a1=",round(a1,3));
+
