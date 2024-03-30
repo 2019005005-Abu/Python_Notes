7dfba4b854ab7d5d4169aa7ead851590ecb96818
Rifat <rifatyou2686@gmail.com>
2024-03-31 00:09:53 +0600

    Class_8 nested-loops and List

---
+++ class_7.py
@@ -0,0 +1,15 @@
+# r W >
+a=10;
+class_7=print;
+result= b='yes' if a==10 else 'No'
+class_7(result);
+
+i=0;
+
+while i<=6:
+     i=i+1
+     class_7('I am a Python Developer\n');
+   
+n=int(input('Digit'))
+class__7=n%2==0 if 'Odd' else 'Even'
+class_7(class__7)
